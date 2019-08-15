479b78404e882d08f3d4dfbf71cccc271de4659f
Park Ju Hyung <staryoman@gmail.com>
2019-08-16 01:14:38 +0900

    add userId field

--- src/main/java/com/kkk26kkk/bbs/model/CommentParam.java
+++ src/main/java/com/kkk26kkk/bbs/model/CommentParam.java
@@ -4,9 +4,11 @@ import com.kkk26kkk.common.model.BaseParam;
 
 public class CommentParam extends BaseParam {
 	private final int articleId;
+	private final String userId;
 	
 	public static class Builder extends BaseParam.Builder<Builder> {
 		private int articleId;
+		private String userId;
 		
 		public Builder(int pageSize, int articleId) {
 			super(pageSize);
@@ -23,6 +25,11 @@ public class CommentParam extends BaseParam {
 			return this;
 		}
 		
+		public Builder userId(String userId) {
+			this.userId = userId;
+			return this;
+		}
+		
 		@Override
 		public CommentParam build() {
 			super.build();
@@ -33,9 +40,14 @@ public class CommentParam extends BaseParam {
 	public CommentParam(Builder builder) {
 		super(builder);
 		this.articleId = builder.articleId;
+		this.userId = builder.userId;
 	}
 
 	public int getArticleId() {
 		return articleId;
+	}
+	
+	public String getUserId() {
+		return userId;
 	}
 }
