ddf654bc5e4d505a3cd3814c37b729886c1f22c9
Park Ju Hyung <staryoman@gmail.com>
2019-08-16 01:14:11 +0900

    add sort field

--- src/main/java/com/kkk26kkk/bbs/model/ArticleParam.java
+++ src/main/java/com/kkk26kkk/bbs/model/ArticleParam.java
@@ -3,9 +3,18 @@ package com.kkk26kkk.bbs.model;
 import com.kkk26kkk.common.model.BaseParam;
 
 public class ArticleParam extends BaseParam {
+	private final String sort;
+	
 	public static class Builder extends BaseParam.Builder<Builder> {
+		private String sort;
+		
 		public Builder(int pageSize) {
 			super(pageSize);
+		}
+		
+		public Builder sort(String sort) {
+			this.sort = sort;
+			return this;
 		}
 		
 		@Override
@@ -17,5 +26,10 @@ public class ArticleParam extends BaseParam {
 	
 	public ArticleParam(Builder builder) {
 		super(builder);
+		this.sort = builder.sort;
+	}
+	
+	public String getSort() {
+		return sort;
 	}
 }
