dee49784eda2815c9eaec5800fa71ef07d267f5b
문한주(163229) <luvet@shinsegae.com>
2019-08-18 16:53:42 +0900

    [2019-08-19] Code review for Feed List AOP

--- src/main/java/com/kkk26kkk/bbs/controller/BoardController.java
+++ src/main/java/com/kkk26kkk/bbs/controller/BoardController.java
@@ -107,15 +107,18 @@ public class BoardController {
 //				.sort(sort)
 				.build();
 		
-		// XXX 글리스트, 댓글리스트를 service단에서 같이 처리해야 할까요?
+        // XXX 글리스트, 댓글리스트를 service단에서 같이 처리해야 할까요?
+        // TODO 글리스트는 서비스에서 DAO 직접 호출하고, 글별 댓글리스트를 붙이는 AOP 개발 (1개, List, PageList 대응)
 		PageList<Article> pageArticleList = boardService.getFeedList(articleParam); 
 		List<Article> articleList = pageArticleList.getList();
 		
-		String articleIdList = articleList.stream()
+        String articleIdList = articleList.stream()
+        // TODO Article에 getArticleId() 오버라이드, super.getArticleId() 호출
 			.map(Article::getArticleId) // XXX getArticleId() -> public으로 열어줘야하지 않을까요?
 			.collect(Collectors.joining(","));
 		
-		// XXX totalPage 방식으로 하려면 totalCount 처리를 어떻게 해야할까요?
+        // XXX totalPage 방식으로 하려면 totalCount 처리를 어떻게 해야할까요?
+        // TODO Param에서 useMore(true) 빼고 useTotal(true) 사용 -> PageList에서 getTotalPage() 사용
 		CommentParam commentParam = new CommentParam
 				.Builder(5, articleIdList)
 				.useMore(true)
@@ -125,7 +128,12 @@ public class BoardController {
 		PageList<Comment> pageCommentList = boardService.getFeedCommentList(commentParam);
 		
 		// XXX 댓글 리스트들이 해당 글 리스트와 짝을 지으려면 어떻게 해야할까요?
-		
+        // TODO PageList<Comment> 조회시 객체의 getArticleId() 기준으로 그룹핑 (Comment에 오버라이드 필요, 그룹기준 외부주입 필요)
+        // Map<String, PageList<Comment>> -> 이후 Map의 key를 기준으로 CommentDto와 ArticleDto를 그룹핑
+        // ArticleDto에 PageList<CommentDto> 추가 또는 Map으로 ArticleDto와 PageList<CommentDto> 조합
+        // 팩토리 객체 추가 고려 -> PageList<Article>과 PageList<Comment>를 각각 PageList<ArticleDto>와 PageList<CommentDto>로 변환하는
+        // VO와 DTO의 필드구조가 많이 달라졌을 때 빛을 발함, 같은 필드더라도 비즈니스 로직에 의해 값이 가공 되었을 때도 빛을 발함
+
 		return "/board/feedList";
 	}
 }
