0ff12331e1c69f9214e06fd8f236ad3fb8f6a1e8
Park Ju Hyung <staryoman@gmail.com>
2019-09-22 12:28:56 +0900

    delete superset methods

--- src/main/java/com/kkk26kkk/bbs/model/ArticleDecorator.java
+++ src/main/java/com/kkk26kkk/bbs/model/ArticleDecorator.java
@@ -7,28 +7,6 @@ public abstract class ArticleDecorator extends Article {
 		this.article = article;
 	}
 	
-	// TODO 슈퍼셋에 있는 메소드들(데코레이팅 대상이 아닌) 여기에서 지울 수 있는 구조 알아보자
-
-	@Override
-	public final String getRootId() {
-		return article.getRootId();
-	}
-
-	@Override
-	public final void setRootId(String rootId) {
-		article.setRootId(rootId);
-	}
-
-	@Override
-	public final String getArticleId() {
-		return article.getArticleId();
-	}
-
-	@Override
-	public final ArticleDto showHeader() {
-		return article.showHeader();
-	}
-	
 	@Override
 	public abstract ArticleDto showContent();
 }
